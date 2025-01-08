4dc60055bd39d5a91eebfdf9fb6b485b87840a0f
Siegerus <ivanoffsiegerus@gmail.com>
2025-01-08 11:12:31 +0300

    catalogToAdapt

--- src/blocks/modules/catalog/catalog.js
+++ src/blocks/modules/catalog/catalog.js
@@ -2,6 +2,7 @@ import SimpleBar from "simplebar";
 import ResizeObserver from "resize-observer-polyfill";
 
 import { toFadeIn } from "../header/header.js";
+import { toFadeOut } from "../header/header.js";
 
 window.addEventListener("DOMContentLoaded", function () {
     if(this.document.querySelector(".catalog")) {
@@ -22,11 +23,19 @@ window.addEventListener("DOMContentLoaded", function () {
             parent = this.document.querySelector(".catalog__tabs"),
             tabs = this.document.querySelectorAll(".catalog__tab"),
             content = this.document.querySelectorAll(".cards__wrapper_ctg"),
-    
+            radio = parent.querySelectorAll("input[type='radio']"),
+
+            selectHeadline = this.document.querySelector(".catalog__select-headline"),
+            selectText = selectHeadline.querySelector(".catalog__select-text"),
+            selectArrow = selectHeadline.querySelector(".catalog__select-icon"),
+
+            
             accordItem = this.document.querySelectorAll(".catalog-accord"),
             accordContent = this.document.querySelectorAll(".catalog-accord__content"),
             scrollBox = this.document.querySelectorAll(".catalog-accord__scroll-box"),
             accordImg = this.document.querySelectorAll(".catalog-accord__icon");
+
+        console.log(radio);
     
         hearts.forEach((item) => {
             item.addEventListener("click", () => {
@@ -51,7 +60,7 @@ window.addEventListener("DOMContentLoaded", function () {
     
         let setTabs = function () {
             parent.addEventListener("click", (e) => {
-                if (e.target || e.target.classList.contains("catalog__tab")) {
+                if (e.target || e.target.classList.closest("catalog__tab")) {
                     for (let i = 0; i < tabs.length; i++) {
                         if (e.target == tabs[i]) {
                             hideContent(0);
@@ -66,9 +75,54 @@ window.addEventListener("DOMContentLoaded", function () {
                     for (let i = 0; i < tabs.length; i++) {
                         tabs[i].classList.remove("catalog__tab_active");
                         this.classList.add("catalog__tab_active");
+                        if (tabs[i].classList.contains("catalog__tab_active")) {
+                            radio[i].checked = true;
+                            selectText.textContent = tabs[i].textContent;
+
+                        }
                     }
                 });
             });
+
+            let setSelect = ()=> {
+
+                let showSelect = function() {
+                    parent.classList.add("catalog__tabs_active");
+                    selectArrow.style.transform = "rotate(180deg)";
+                    toFadeIn(parent);
+                };
+
+                let hideSelect = function() {
+                    selectArrow.style.transform = "rotate(0deg)";
+                    toFadeOut(parent, "catalog__tabs_active");
+                };
+                
+                let hideByOverclick = function(e) {
+                    if (e.target.closest(".catalog__tabs") || e.target.closest(".catalog__select-headline")) {
+                        return;
+                    }
+                    hideSelect();
+                    selectHeadline.classList.remove("catalog__select-headline_active");
+                    document.removeEventListener("click", hideByOverclick);
+                    
+                };
+
+                selectHeadline.addEventListener("click", function(e)  {
+                    if (e.target.closest(".catalog__select-headline")) {
+                        this.classList.toggle("catalog__select-headline_active");
+                    }
+                    if (selectHeadline.classList.contains("catalog__select-headline_active")) {
+                        showSelect();
+                        setTimeout(()=> document.addEventListener("click", hideByOverclick));
+                        
+                    } else {
+                        hideSelect();
+                        document.removeEventListener("click", hideByOverclick);
+                    }
+                });
+            };
+
+            setSelect();
         };
     
         setTabs();
@@ -123,5 +177,4 @@ window.addEventListener("DOMContentLoaded", function () {
     
         setAccordeon();
     }
-    
 });
